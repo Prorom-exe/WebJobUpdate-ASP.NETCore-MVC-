a689374a80b09de4fe6c896e5ade860679388b4f
Prorom-exe <69636622+Prorom-exe@users.noreply.github.com>
2020-10-16 15:02:51 +0300

    Добавьте файлы проекта.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebJob.Models;
+
+namespace WebJob.Controllers
+{
+    
+    
+        public class ProductController : Controller
+        {
+            private IProductRepository repository;
+
+            public ProductController(IProductRepository repo)
+            {
+                repository = repo;
+            }
+            public ViewResult List() => View(repository.Products);
+        }
+    
+}
